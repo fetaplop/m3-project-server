819694565a6fae3000e72ce12cb24aea9eca306f
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-05-21 18:59:32 +0300

    changed lat. and lon. order in Stop model to match  GTFS format

--- models/Stop.js
+++ models/Stop.js
@@ -5,8 +5,8 @@ const stopSchema = new Schema({
     name: {type: String, required: true},
     stopId: {type: Number, required: true},
     stopCode: {type: Number, required: true},
-    longitude: {type: Number, required: true},
     latitude: {type: Number, required: true}, // would be smart (?) to wrap these 2 together but they come as separate properties from the api
+    longitude: {type: Number, required: true},
     busLines: [{type: Number}]
     //    busLines: [{type: mongoose.Schema.Types.ObjectId, ref: "Line"}] // might need to have this if the database grows
 })
