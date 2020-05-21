2af7bf7bbe1a3f826e928524eff54556ccf5a2ab
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-05-21 22:49:59 +0300

    put method is trash, POST method for adding to user favourites working

--- routes/stopRouter.js
+++ routes/stopRouter.js
@@ -4,6 +4,7 @@ const stopRouter = express.Router();
 const createError = require("http-errors");
 
 const Stop = require("../models/Stop");
+const User = require("../models/User")
 
 // IMPORTANT: probably should only get the STATIC data once per login. how do we handle this?
 // GET    '/stops/'
@@ -34,28 +35,103 @@ stopRouter.get("/:id", (req, res, next) => {
 
 })
 
+// THIS IS THE ONLY ONE THAT SEEMS TO BE WORKING
+stopRouter.post("/:id/save", (req, res, next) => {
+    const {id} = req.params;
+
+    // I promise to find user and update favStops 
+    User.findByIdAndUpdate(
+        { _id: req.session.currentUser._id },
+        { $push: { favStops: id } } // for unlike just the same with $pull ??
+      )
+      .then((userUpdated) => {
+          console.log('userUpdated', userUpdated)
+          res.status(201)
+          .json(userUpdated)
+      })
+      .catch((err) => next(createError(err)))
+
+    
+})
+
 // PUT    '/stops/:id/save'
 stopRouter.put("/:id/save", (req, res, next) => {
     // stop id in collection
     const {id} = req.params;
+
+    // Stop.findById(id)
+    //     .then((stop) => {
+    //         console.log('stop obj that we are trying to add to favourites: ', stop)
+
+    //         // this requires a check if we are logged in or no????
+    //         const user = req.session.currentUser;
+    //         console.log('user before adding new fav stop:', user)
+
+
+    //         // actually can I really do this???
+    //         user.favStops.push(stop)
+    //         console.log('user.favStops after pushing new stop to favourites: ', user.favStops)
+    //         res.status(201) // should be  201 (Created)
+    //             .json(stop) // could be removed maybe
+    //     })
+    //     .catch((err) => next(createError(err)))
+
+    // --------------------------------- try again: ---------------------------------------
 
     Stop.findById(id)
         .then((stop) => {
             console.log('stop obj that we are trying to add to favourites: ', stop)
 
             // this requires a check if we are logged in or no????
-            const user = req.session.currentUser;
-            console.log('user before adding new fav stop:', user)
+            const userID = req.session.currentUser._id;
+            //console.log('user before adding new fav stop:', user)
 
-            user.favStops.push(stop)
-            console.log('user.favStops after pushing new stop to favourites: ', user.favStops)
+            return User.findByIdAndUpdate({userID}, {$push: {favStops: stop._id}})
+        })
+        .then(userUpdated => {
+            console.log('userUpdated: ', userUpdated)
             res.status(201) // should be  201 (Created)
-                .json(stop) // could be removed maybe
+                .json(userUpdated) // could be removed maybe
         })
         .catch((err) => next(createError(err)))
+
+    // --------------------------another way: (chained promises)-------------------------------
+    // let promiseStop = Stop.findById(id)
+    //     .then((stop) => {
+    //         console.log('stop obj that we are trying to add to favourites: ', stop)
+    //     })
+    //     .catch((err) => next(createError(err)))
+
+    // let promiseUser = User.findByIdAndUpdate({_id: req.session.currentUser._id}, {$push: {favStops: stop._id}})
+    //     .then((userUpdated) => {
+    //         console.log('userUpdated is the second promise: ', userUpdated)
+    //     })
+    //     .catch((err) => next(createError(err)))
+
+    // Promise.all([promiseStop, promiseUser])
+    //     .then(resultArr => {
+    //         console.log('resultArray from promise all: ', resArr)
+    //         res.status(201)
+    //         .json(resultArr)
+    //     })
+    //     .catch((err) => next(createError(err)))
+
+
 })
 
 // PUT    '/stops/:id/unsave'
+stopRouter.put("/:id/unsave", (req, res, next) => {
+    // stop id in collection
+    const {id} = req.params;
 
+    Stop.findById(id)
+        .then((stop) => {
+            const user = req.session.currentUser
+            console.log('user.favStops before we try to remove anything from there', user.favStops)
+            console.log(user.favStops.indexOf(stop._id))
+            res.json(stop)
+        })
+        .catch((err) => next(createError(err)))
+})
 
 module.exports = stopRouter
