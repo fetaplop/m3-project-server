b553586672af591313de854e14b1b96448a9c10a
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-05-26 15:17:52 +0300

    in save stop by id, check if the stop id is already in faves and dont add it again. this maybe works now

--- routes/stopRouter.js
+++ routes/stopRouter.js
@@ -41,19 +41,53 @@ stopRouter.get("/:id", (req, res, next) => {
 // POST   '/stops/:id/save'
 stopRouter.post("/:id/save", isLoggedIn, (req, res, next) => { //isLoggedin!!!
     const {id} = req.params;
+    const userID = req.session.currentUser._id
 
-    // I promise to find user by id and update favStops 
-    User.findByIdAndUpdate(
-        {_id: req.session.currentUser._id},
-        {$push: {favStops: id}} // for unlike just the same with $pull ??
-    )
-        .then((userUpdated) => {
+    //check if the stop IS already fave => if yes, dont' allow another save || if no, add it to favourites
+
+    User.findById(userID)//_id: req.session.currentUser._id
+        .then((userFound) => {
+            console.log('userFound', userFound)
+            console.log('userFound.favStops', userFound.favStops)
+            console.log('this id is supposed to be the stop id, just checking if its still visible in user.findbyId', id)
+            console.log('typeof id', typeof id)
+            if (userFound.favStops.includes(id)) {
+                // this is already in userfaves, do not add again!
+                throw new Error("Stop is already in user favourites, will not add it again")
+            }
+            else { // add stop to userFavourites
+                return (User.updateOne( // return a promise of updating this USer
+                    {_id: userID},
+                    {$push: {favStops: id}},
+                    {new: true} // new: true => return updated version of user HAD TO TAKE THIS OUT
+
+                ))
+                // try returning whole user and update it in next then block
+                // return userFound
+            }
+        })
+        // .then((userToUpdate) => {
+        //     User.updateOne( // return a promise of updating this USer
+        //         {_id: userToUpdate._id},
+        //         {$push: {favStops: id}},
+        //         {new: true}
+        //     )}
+        // )
+        .then((userUpdated) => { // we go to this block if we succeeded in updating the user
             console.log('userUpdated', userUpdated)
             res.status(201)
                 .json(userUpdated)
         })
         .catch((err) => next(createError(err)))
 
+
+    // I promise to find user by id and update favStops 
+    ////////////////////// old version with no chekcking if stop is already a favourite
+    // User.findByIdAndUpdate(
+    //     {_id: req.session.currentUser._id},
+    //     {$push: {favStops: id}}, // for unlike just the same with $pull ??
+    //     {new: true} //returns updtaed version of user
+    // )
 })
 
 // POST   '/stops/:id/UNsave'
@@ -63,12 +97,13 @@ stopRouter.post("/:id/unsave", isLoggedIn, (req, res, next) => {
     // I promise to find user and update favStops 
     User.findByIdAndUpdate(
         {_id: req.session.currentUser._id},
-        {$pull: {favStops: id}} // for unlike $pull 
+        {$pull: {favStops: id}}, // for unlike $pull 
+        {new: true} //returns updtaed version of user
     )
         .then((userUpdated) => {
             console.log('userUpdated', userUpdated)
             res.status(200)
-                .json(userUpdated)
+                .json(userUpdated) // this shows whatever!!
         })
         .catch((err) => next(createError(err)))
 
@@ -81,84 +116,84 @@ stopRouter.post("/:id/unsave", isLoggedIn, (req, res, next) => {
 // -------------------------------------------------------------------------------------------------------
 // THESE DO NOT WORK except one of them, almost... leaving them for now
 // PUT    '/stops/:id/save'
-stopRouter.put("/:id/save", (req, res, next) => {
-    // stop id in collection
-    const {id} = req.params;
+// stopRouter.put("/:id/save", (req, res, next) => {
+//     // stop id in collection
+//     const {id} = req.params;
 
-    // THIS WAY IT WORKS BUT UPDATES DO NOT SHOW UP IN COMPASS
-    // Stop.findById(id)
-    //     .then((stop) => {
-    //         console.log('stop obj that we are trying to add to favourites: ', stop)
+//     // THIS WAY IT WORKS BUT UPDATES DO NOT SHOW UP IN COMPASS
+//     // Stop.findById(id)
+//     //     .then((stop) => {
+//     //         console.log('stop obj that we are trying to add to favourites: ', stop)
 
-    //         // this requires a check if we are logged in or no????
-    //         const user = req.session.currentUser;
-    //         console.log('user before adding new fav stop:', user)
-
-
-    //         // actually can I really do this???
-    //         user.favStops.push(stop)
-    //         console.log('user.favStops after pushing new stop to favourites: ', user.favStops)
-    //         res.status(201) // should be  201 (Created)
-    //             .json(stop) // could be removed maybe
-    //     })
-    //     .catch((err) => next(createError(err)))
-
-    // --------------------------------- try again: ---------------------------------------
-
-    Stop.findById(id)
-        .then((stop) => {
-            console.log('stop obj that we are trying to add to favourites: ', stop)
-
-            // this requires a check if we are logged in or no????
-            const userID = req.session.currentUser._id;
-            //console.log('user before adding new fav stop:', user)
-
-            return User.findByIdAndUpdate({userID}, {$push: {favStops: stop._id}})
-        })
-        .then(userUpdated => {
-            console.log('userUpdated: ', userUpdated)
-            res.status(201) // should be  201 (Created)
-                .json(userUpdated) // could be removed maybe
-        })
-        .catch((err) => next(createError(err)))
-
-    // --------------------------another way: (chained promises)-------------------------------
-    // let promiseStop = Stop.findById(id)
-    //     .then((stop) => {
-    //         console.log('stop obj that we are trying to add to favourites: ', stop)
-    //     })
-    //     .catch((err) => next(createError(err)))
-
-    // let promiseUser = User.findByIdAndUpdate({_id: req.session.currentUser._id}, {$push: {favStops: stop._id}})
-    //     .then((userUpdated) => {
-    //         console.log('userUpdated is the second promise: ', userUpdated)
-    //     })
-    //     .catch((err) => next(createError(err)))
-
-    // Promise.all([promiseStop, promiseUser])
-    //     .then(resultArr => {
-    //         console.log('resultArray from promise all: ', resArr)
-    //         res.status(201)
-    //         .json(resultArr)
-    //     })
-    //     .catch((err) => next(createError(err)))
+//     //         // this requires a check if we are logged in or no????
+//     //         const user = req.session.currentUser;
+//     //         console.log('user before adding new fav stop:', user)
 
 
-})
+//     //         // actually can I really do this???
+//     //         user.favStops.push(stop)
+//     //         console.log('user.favStops after pushing new stop to favourites: ', user.favStops)
+//     //         res.status(201) // should be  201 (Created)
+//     //             .json(stop) // could be removed maybe
+//     //     })
+//     //     .catch((err) => next(createError(err)))
 
-// PUT    '/stops/:id/unsave'
-stopRouter.put("/:id/unsave", (req, res, next) => {
-    // stop id in collection
-    const {id} = req.params;
+//     // --------------------------------- try again: ---------------------------------------
 
-    Stop.findById(id)
-        .then((stop) => {
-            const user = req.session.currentUser
-            console.log('user.favStops before we try to remove anything from there', user.favStops)
-            console.log(user.favStops.indexOf(stop._id))
-            res.json(stop)
-        })
-        .catch((err) => next(createError(err)))
-})
+//     Stop.findById(id)
+//         .then((stop) => {
+//             console.log('stop obj that we are trying to add to favourites: ', stop)
+
+//             // this requires a check if we are logged in or no????
+//             const userID = req.session.currentUser._id;
+//             //console.log('user before adding new fav stop:', user)
+
+//             return User.findByIdAndUpdate({userID}, {$push: {favStops: stop._id}})
+//         })
+//         .then(userUpdated => {
+//             console.log('userUpdated: ', userUpdated)
+//             res.status(201) // should be  201 (Created)
+//                 .json(userUpdated) // could be removed maybe
+//         })
+//         .catch((err) => next(createError(err)))
+
+//     // --------------------------another way: (chained promises)-------------------------------
+//     // let promiseStop = Stop.findById(id)
+//     //     .then((stop) => {
+//     //         console.log('stop obj that we are trying to add to favourites: ', stop)
+//     //     })
+//     //     .catch((err) => next(createError(err)))
+
+//     // let promiseUser = User.findByIdAndUpdate({_id: req.session.currentUser._id}, {$push: {favStops: stop._id}})
+//     //     .then((userUpdated) => {
+//     //         console.log('userUpdated is the second promise: ', userUpdated)
+//     //     })
+//     //     .catch((err) => next(createError(err)))
+
+//     // Promise.all([promiseStop, promiseUser])
+//     //     .then(resultArr => {
+//     //         console.log('resultArray from promise all: ', resArr)
+//     //         res.status(201)
+//     //         .json(resultArr)
+//     //     })
+//     //     .catch((err) => next(createError(err)))
+
+
+// })
+
+// // PUT    '/stops/:id/unsave'
+// stopRouter.put("/:id/unsave", (req, res, next) => {
+//     // stop id in collection
+//     const {id} = req.params;
+
+//     Stop.findById(id)
+//         .then((stop) => {
+//             const user = req.session.currentUser
+//             console.log('user.favStops before we try to remove anything from there', user.favStops)
+//             console.log(user.favStops.indexOf(stop._id))
+//             res.json(stop)
+//         })
+//         .catch((err) => next(createError(err)))
+// })
 
 module.exports = stopRouter
